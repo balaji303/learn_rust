f9e8cb5d4130d29b06421346db59db964a5bb1dd
Balaji <balajimail@zoho.com>
2023-06-01 07:47:09 +0530

    I took my chick up to the North, yeah (badass *itch)

---
+++ example/08_For/main.rs
@@ -0,0 +1,8 @@
+fn main()
+{
+    // let a = [1,3];
+    for loop_count in 1..4
+    {
+        println!("loop_count: {}",loop_count);
+    }
+}
---
+++ example/08_For/main_v2.rs
@@ -0,0 +1,9 @@
+fn main()
+{
+    // New method of assigning values
+    let a = [1,4];
+    for loop_count in a.iter()
+    {
+        println!("loop_count: {}",loop_count);
+    }
+}
